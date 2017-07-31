73a9587749a9dd5402290c5cacda25c22a2c8ab9
Ldrumstick <liaochengtao@foxmail.com>
2017-07-31 16:03:35 +0800

    创建脚本
    创建了用于记录人物状态信息的脚本

---
+++ Assets/Scripts/Player/Player_State.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 用于记录人物的状态信息
+/// </summary>
+public class Player_State : MonoBehaviour {
+
+    public int level = 1;  //等级
+    public int HP = 100;   //血量
+    public int MP = 100;   //魔力值
+    public int gold = 200; //金币数
+
+
+	
+}
